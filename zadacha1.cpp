0afddc5d1cf4d39d34491b6fa8ba229a6689aaba
Pavel Nikolaev <altertumlicher@gmail.com>
2025-03-08 10:29:56 +0500

    initial commit

---
+++ zadacha1.cpp
@@ -0,0 +1,94 @@
+﻿#include <iostream>
+
+class Calculator
+{
+private:
+
+    double num1{};
+    double num2{};
+
+public:
+
+    Calculator(double num1, double num2)
+    {
+        num1 = set_num1(num1);
+        num2 = set_num2(num2);
+        std::cout << "num1 + num2 = " << add() << std::endl;
+        std::cout << "num1 - num2 = " << subtract_1_2() << std::endl;
+        std::cout << "num2 - num1 = " << subtract_2_1() << std::endl;
+        std::cout << "num1 * num2 = " << multiply() << std::endl;
+        std::cout << "num1 / num2 = " << divide_1_2() << std::endl;
+        std::cout << "num2 / num1 = " << divide_2_1() << std::endl;
+    }
+
+private:
+
+    double add() { return num1 + num2; }
+    double multiply() { return num1 * num2; }
+    double subtract_1_2() { return num1 - num2; }
+    double subtract_2_1() { return num2 - num1; }
+    double divide_1_2() { return num1 / num2; }
+    double divide_2_1() { return num2 / num1; }
+    bool set_num1(double num1)
+    {
+        if (true)
+        {
+            this->num1 = num1;
+        }
+        return true;
+    }
+
+    bool set_num2(double num2)
+    {
+        if (true)
+        {
+            this->num2 = num2;
+        }
+        return true;
+    }
+
+public:
+    ~Calculator(){}
+
+};
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    double num1{};
+    double num2{};
+
+    bool running{true};
+
+    while (running)
+    {
+        std::cout << "Введите num1: ";
+        std::cin >> num1;
+
+        while (num1 == 0)
+        {
+            std::cout << "Неверный ввод!" << std::endl;
+            std::cout << "Введите num1: ";
+            std::cin >> num1;
+        }
+
+        std::cout << "Введите num2: ";
+        std::cin >> num2;
+
+        while (num2 == 0)
+        {
+            std::cout << "Неверный ввод!" << std::endl;
+            std::cout << "Введите num2: ";
+            std::cin >> num2;
+        }
+
+        Calculator calculator(num1, num2);
+    }
+
+    return 0;
+}
+
+
---
+++ zadacha2.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <Windows.h>
+
+class Counter
+{
+private:
+
+	int num{};
+
+public:
+
+	Counter(int setNum) : num{ setNum } {}
+
+	int counterPlus(int setNum) { return ++setNum; }
+	int counterMinus(int setNum) { return --setNum; }
+	int counterEqual(int setNum) { return setNum; }
+
+private:
+
+	void setNum(int setNum) { num = setNum; }
+
+public:
+
+	~Counter(){}
+};
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int num{};
+	char sign{};
+	std::string userInput{};
+
+	std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+	std::cin >> userInput;
+
+	if (userInput == "нет")
+	{
+		std::cout << "До свидания!";
+		return 0;
+	}
+
+	std::cout << "Введите начальное значение счётчика: ";
+	std::cin >> num;
+
+	Counter counter(num);
+
+	std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+	std::cin >> sign;
+
+	while(sign != 'x' && sign != 'х')
+	{ 
+		switch (sign)
+		{
+		case '+':
+			num = counter.counterPlus(num);
+			break;
+		case '-':
+			num = counter.counterMinus(num);
+			break;
+		case '=':
+			num = counter.counterEqual(num);
+			std::cout << num << std::endl;
+			break;
+		}
+		std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+		std::cin >> sign;
+	}
+	std::cout << "До свидания!";
+
+	return 0;
+}
